b8344b38a4a5fcb7922f446c90f28e954721a4e1
OMOTO Kenji <doloopwhile@gmail.com>
2017-06-25 23:25:10 +0900

    tmp

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,19 @@
 class TopController < ApplicationController
+  include Magick
+
   def show
+    im = Image.read('/home/kenjiomoto/Desktop/girl_skin.png').first
+    
+    data = im.rows.times.flat_map { |r| 
+      im.export_pixels(0, r, im.columns, 1, 'RGBA').map {|x| x / 256 }
+    }
+  
+    @skin = {
+      width: 64,
+      height: 64,
+      layers: [
+        { data: data }
+      ]
+    }
   end
 end
--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -1,57 +1,76 @@
-// Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
-// like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
-// of the page.
-
 import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 import Immutable from 'immutable'
 
-const style = {
-  width: '200px',
-  height: '200px',
-  border: '1px solid black',
-  background: 'lightgray',
-  margin: '10px'
-};
 
-// class Layers {
-//   // constructor
-// }
-// class Skin {
-//   constructor(layers) {
-//     this.layers = layers;
-//   }
-//   insertLayer(i, l) {
-//     this.layers.add(l);
-//   }
-// }
 
-class ImageView extends React.Component {
-  render() {
-    const refs = (element) => {
-      if (element == null) { return; }
+const copyArrayElements = (arrDst, arrSrc) => {
+  for(var k = 0; k < arrSrc.length; ++k) {
+    arrDst[k] = arrSrc[k];
+  }
+}
 
+const ImageView = props => {
+  const h = props.height;
+  const w = props.width;
+
+  const refs = (element) => {
+    if (element == null) { return; }
+
+    const skin = props.skin;
+
+    for(var i = 0; i < skin.layers.length; ++i) {
       const canvas = document.createElement('canvas')
-      canvas.height = 200;
-      canvas.width = 200;
+      canvas.height = skin.height;
+      canvas.width  = skin.width;
+
       const ctx = canvas.getContext('2d');
-      const sh = 200;
-      const sw = 200;
-      const im = ctx.createImageData(sw, sh);
-
-      for(var i = 0; i < 4 * 200 * 200; ++i) {
-        im.data[i] = this.props.imageData.get(i);
-      }
-
+      const im = ctx.createImageData(skin.width, skin.height);
+      copyArrayElements(im.data, skin.layers[i].data)
       ctx.putImageData(im, 0, 0);
     
       var context = element.getContext('2d');
-      context.clearRect(0, 0, 800, 800);
-      context.drawImage(canvas, 0, 0, 800, 800);
+      context.clearRect(0, 0, w, h);
+      context.imageSmoothingEnabled = false;
+      context.drawImage(canvas, 0, 0, w, h);
     }
-    return <canvas ref={refs} width={800} height={800} />;
   }
+
+  return <canvas ref={refs} width={w} height={h} />;
+}
+
+const Preview = props => {
+  const style = {
+    width: '64px',
+    height: '64px',
+    border: '1px solid black',
+    background: 'lightgray',
+    margin: '10px',
+    cursor: 'default'
+  };
+  return (
+    <div style={style}>
+      <ImageView width={64} height={64} skin={props.skin}/>
+    </div>
+  )
+}
+
+let putPixels = (skin, i, pixels) => {
+  const data = Array.from(skin.layers[i].data);
+  pixels.forEach((item) => {
+    const x = item[0]
+    const y = item[1]
+    const rgba = item[2]
+    const i = 4 * (x + y * skin.width)
+    data[i]     = rgba[0];
+    data[i + 1] = rgba[1];
+    data[i + 2] = rgba[2];
+    data[i + 3] = rgba[3];
+  });
+  const layers = Array.from(skin.layers)
+  layers[i] = { data: data }      
+  return { width: skin.width, height: skin.height, layers: layers }
 }
 
 
@@ -59,56 +78,106 @@ class ImageEdit extends React.Component {
   constructor(props) {
     super(props)
 
-    var data = new Array(4 * 200 * 200);
-    for(var y = 0; y < 200; ++y) {
-      for(var x = 0; x < 200; ++x) {
-        const i = 4 * (x + y * 200)
-        data[i] = 0;
-        data[i + 1] = 0;
-        data[i + 2] = 0;
-        data[i + 3] = 255;        
-      }
-    }
     this.state = {
-      imageData: Immutable.List(data)
+      prevPos: null,
+      scale: 20
     }
-  }
-
-  onMouseDown(event) {
-    event.preventDefault()
-
   }
 
   render() {
-    let onClick = (e) => {
+    let onMouseDown = (e) => {
       e.preventDefault();
-      const x = Math.floor(e.nativeEvent.offsetX / 4)
-      const y = Math.floor(e.nativeEvent.offsetY / 4)
-      
-      const i = 4 * (x + y * 200)
+      if (e.nativeEvent.which !== 1) { return }
+      const x = Math.floor(e.nativeEvent.offsetX / this.state.scale)
+      const y = Math.floor(e.nativeEvent.offsetY / this.state.scale)
 
-      const imageData = this.state.imageData.set(i, 255).set(i + 1, 255).set(i + 2, 255)
-      
-      this.setState({ imageData: imageData })
+      const skin = putPixels(this.props.skin, 0, [[x, y, [255, 0, 255, 255]]])
+      this.setState({prevPos: [x, y]})
+      this.props.onSkinChange(skin)   
     }
 
+    let onMouseMove = (e) => {
+      e.preventDefault();
+      if (this.state.prevPos == null) { return }
+      if (e.nativeEvent.which !== 1) { return }
+
+      const x = Math.floor(e.nativeEvent.offsetX / this.state.scale)
+      const y = Math.floor(e.nativeEvent.offsetY / this.state.scale)
+
+      const px = this.state.prevPos[0]
+      const py = this.state.prevPos[1]
+
+      const pixels = []
+      pixels.push([x, y, [255, 0, 255, 255]])
+      const m = Math.abs(x - px) + Math.abs(y - py)
+      for (var i = 1; i < m; i++) {
+        const x1 = Math.floor(((m - i) * x + i * px) / m)
+        const y1 = Math.floor(((m - i) * y + i * py) / m)
+        pixels.push([x1, y1, [255, 0, 255, 255]])        
+      }
+      pixels.push([px, py, [255, 0, 255, 255]])
+
+      const skin = putPixels(this.props.skin, 0, pixels)
+      this.setState({ prevPos: [x, y] })
+      this.props.onSkinChange(skin)   
+    }
+
+    let onMouseUp = (e) => {
+      if (e.nativeEvent.which !== 1) { return }
+      this.setState({prevPos: null})
+    }
+    let onMouseLeave = (e) => {
+      if (e.nativeEvent.which !== 1) { return }
+      this.setState({prevPos: null})
+    }
+
+    const style = {
+      width: (64 * this.state.scale) + 'px',
+      height: (64 * this.state.scale) + 'px',
+      border: '1px solid black',
+      background: 'lightgray',
+      margin: '10px',
+      cursor: 'default'
+    };
+
     return (
-      <div style={style} onClick={onClick}>
-        <ImageView imageData={this.state.imageData} />
+      <div style={style}
+        onMouseDown={onMouseDown}
+        onMouseMove={onMouseMove}
+        onMouseUp={onMouseUp}
+        onMouseLeave={onMouseLeave}
+      >
+        <ImageView skin={this.props.skin} width={(64 * this.state.scale)} height={(64 * this.state.scale)}/>
       </div>
     );
   }
 }
 
-const Editor = props => (
-  <div>
-    <ImageEdit />
-  </div>
-)
+class Editor extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      skin: props.skin
+    }
+  }
+  onSkinChange(skin) {
+    this.setState({skin: skin})
+  }
+  render() {
+    return (
+      <div>
+        <Preview skin={this.state.skin} />
+        <ImageEdit skin={this.state.skin}
+          onSkinChange={this.onSkinChange.bind(this)}
+        />
+      </div>
+    )
+  }
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   const node = document.getElementById('editor')
-  const data = JSON.parse(node.getAttribute('data'))
+  const skin = JSON.parse(node.getAttribute('skin'))
 
-  ReactDOM.render(<Editor {...data} />, node)
+  ReactDOM.render(<Editor skin={skin} />, node)
 })
