06de810fb50f9c9c4dbe689eeba05c56f84466df
OMOTO Kenji <doloopwhile@gmail.com>
2017-07-16 22:42:37 +0900

    tmp

--- app/controllers/editors_controller.rb
+++ app/controllers/editors_controller.rb
@@ -2,26 +2,100 @@ class EditorsController < ApplicationController
   include Magick
 
   def show
-    im = Image.read('/home/kenjiomoto/Desktop/girl_skin.png').first
-    
-    data = im.rows.times.flat_map { |r| 
-      im.export_pixels(0, r, im.columns, 1, 'RGBA').map {|x| x / 256 }
-    }
-  
-    @skin = {
+    load_image_data = lambda do |f|
+      im = Image.read(f).first
+      im.rows.times.flat_map { |r| 
+        im.export_pixels(0, r, im.columns, 1, 'RGBA').map {|x| x / 256 }
+      }
+    end
+
+    @props = {}
+
+    @props[:skin] = {
       width: 64,
       height: 64,
       layers: [
-        { data: data }
+        { 
+          label: 'あたま',
+          data: load_image_data['/home/kenjiomoto/Desktop/skin1.png'],
+          visible: true
+        },
+        {
+          label: 'からだ',
+          data: load_image_data['/home/kenjiomoto/Desktop/skin2.png'],
+          visible: false
+        }
       ]
     }
+
+    hsl = lambda do |h, s, l|
+      c = ColorMath::HSL.new(h, s, l)
+      [c.red * 255, c.green * 255, c.blue * 255].map(&:to_i)
+    end
+
+    colors = [
+      hsl[3, 1.0, 0.3],
+      hsl[3, 1.0, 0.4],
+      hsl[3, 1.0, 0.5],
+      hsl[3, 1.0, 0.6],
+      hsl[3, 1.0, 0.7],
+      hsl[3, 1.0, 0.8],
+
+      hsl[58, 1.0, 0.3],
+      hsl[58, 1.0, 0.4],
+      hsl[58, 1.0, 0.5],
+      hsl[58, 1.0, 0.6],
+      hsl[58, 1.0, 0.7],
+      hsl[58, 1.0, 0.8],
+
+      hsl[150, 0.7, 0.3],
+      hsl[150, 0.7, 0.4],
+      hsl[150, 0.7, 0.5],
+      hsl[150, 0.7, 0.6],
+      hsl[150, 0.7, 0.7],
+      hsl[150, 0.7, 0.8],
+
+      hsl[224, 0.8, 0.3],
+      hsl[224, 0.8, 0.4],
+      hsl[224, 0.8, 0.5],
+      hsl[224, 0.8, 0.6],
+      hsl[224, 0.8, 0.7],
+      hsl[224, 0.8, 0.8],
+
+      hsl[312, 1.0, 0.3],
+      hsl[312, 1.0, 0.4],
+      hsl[312, 1.0, 0.5],
+      hsl[312, 1.0, 0.6],
+      hsl[312, 1.0, 0.7],
+      hsl[312, 1.0, 0.8],
+
+      hsl[0, 0.0, 0.15],
+      hsl[0, 0.0, 0.25],
+      hsl[0, 0.0, 0.4],
+      hsl[0, 0.0, 0.6],
+      hsl[0, 0.0, 0.75],
+      hsl[0, 0.0, 0.95],
+
+      hsl[18, 0.5, 0.1],
+      hsl[18, 0.5, 0.2],
+      hsl[18, 0.5, 0.3],
+      hsl[18, 0.5, 0.4],
+      hsl[18, 0.5, 0.5],
+      hsl[18, 0.5, 0.55],
+
+      hsl[5, 1, 0.8],
+      hsl[5, 1, 0.85],
+      hsl[5, 1, 0.9],
+      hsl[23, 0.7, 0.8],
+      hsl[23, 0.7, 0.85],
+      hsl[23, 0.7, 0.9],
+    ]
+    @props[:colors] = colors
   end
   
   def create
     name = params[:name]
     skin = params[:skin]
-    p name, skin
-    p "SAVED!!!"
     render plain: 'Saved!', layout: false
   end
 end
--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -11,29 +11,100 @@ const copyArrayElements = (arrDst, arrSrc) => {
   }
 }
 
+const PaletteView = props => {
+  const colors = props.colors;
+  const colorIndex = props.colorIndex;
+  const rowCount = 8;
+  const colCount = 6;
+
+  const rows = [];
+  const indexes = [];
+  let i = 0;
+  for (var y = 0; y < rowCount; ++y) {
+    const cols = [];
+    for (var x = 0; x < colCount; ++x) {
+      cols.push(i);
+      ++i;
+    }
+    rows.push(cols);
+  }
+
+  return <table className="palette">
+    {
+      rows.map(function(cols) {
+        return <tr>
+          {
+            cols.map(function(i) {
+              const style = {
+                backgroundColor: "rgb(" + colors[i][0] + "," + colors[i][1] + "," + colors[i][2] + ")"
+              };
+              if (i == colorIndex) {
+                style.borderColor = 'black'
+              }
+              
+              return <td
+                style={style}
+                onClick={() => props.changeColorIndex(i)}
+              >{i}</td>;
+            })
+          }
+        </tr>
+      })
+    }
+  </table>;
+}
+
 const ImageView = props => {
-  const h = props.height;
-  const w = props.width;
+  const skin = props.skin;
+  const scale = props.scale;
+  const h = scale * skin.height;
+  const w = scale * skin.width;
 
   const refs = (element) => {
     if (element == null) { return; }
 
-    const skin = props.skin;
+    const ctx = element.getContext('2d');
+    ctx.fillStyle = "rgba(255, 0, 255, 1)";
+    ctx.fillRect(0, 0, w, h);
 
-    for(var i = 0; i < skin.layers.length; ++i) {
-      const canvas = document.createElement('canvas')
-      canvas.height = skin.height;
-      canvas.width  = skin.width;
-
-      const ctx = canvas.getContext('2d');
-      const im = ctx.createImageData(skin.width, skin.height);
-      copyArrayElements(im.data, skin.layers[i].data)
-      ctx.putImageData(im, 0, 0);
+    ctx.imageSmoothingEnabled = false;
     
-      var context = element.getContext('2d');
-      context.clearRect(0, 0, w, h);
-      context.imageSmoothingEnabled = false;
-      context.drawImage(canvas, 0, 0, w, h);
+    for(let i = skin.layers.length - 1; i >= 0; --i) {
+      const l = skin.layers[i];
+      if (!l.visible) { continue; }
+
+      const layerCanvas = document.createElement('canvas');
+      layerCanvas.height = skin.height;
+      layerCanvas.width  = skin.width;
+
+      const layerContext = layerCanvas.getContext('2d');
+      const im = layerContext.createImageData(skin.width, skin.height);
+      copyArrayElements(im.data, l.data)
+      layerContext.putImageData(im, 0, 0);
+
+      ctx.drawImage(layerCanvas, 0, 0, w, h);
+    }
+
+    if (props.rendersGrid) {
+      const gridCanvas = document.createElement('canvas');
+      gridCanvas.width  = element.width;
+      gridCanvas.height = element.height;
+      
+      const gridContext = gridCanvas.getContext('2d');
+
+      gridContext.strokeStyle = '#FF0000';
+      gridContext.beginPath();
+      for(var x = 1; x < skin.width; ++x) {
+        gridContext.moveTo(x * scale, 0);
+        gridContext.lineTo(x * scale, skin.height * scale);
+      }
+      for(var y = 1; y < skin.height; ++y) {
+        gridContext.moveTo(0,                  y * scale);
+        gridContext.lineTo(skin.width * scale, y * scale);
+      }
+      gridContext.stroke();
+
+      ctx.drawImage(gridCanvas, 0, 0);
     }
   }
 
@@ -51,28 +122,60 @@ const Preview = props => {
   };
   return (
     <div style={style}>
-      <ImageView width={64} height={64} skin={props.skin}/>
+      <ImageView scale={1} skin={props.skin}/>
     </div>
   )
 }
 
-let putPixels = (skin, i, pixels) => {
-  const data = Array.from(skin.layers[i].data);
-  pixels.forEach((item) => {
-    const x = item[0]
-    const y = item[1]
-    const rgba = item[2]
-    const i = 4 * (x + y * skin.width)
-    data[i]     = rgba[0];
-    data[i + 1] = rgba[1];
-    data[i + 2] = rgba[2];
-    data[i + 3] = rgba[3];
-  });
-  const layers = Array.from(skin.layers)
-  layers[i] = { data: data }      
-  return { width: skin.width, height: skin.height, layers: layers }
+const Skin = {
+  putPixels(skin, i, pixels) {
+    const data = Array.from(skin.layers[i].data);
+    pixels.forEach((item) => {
+      const x = item[0]
+      const y = item[1]
+      const rgba = item[2]
+      const i = 4 * (x + y * skin.width)
+      data[i]     = rgba[0];
+      data[i + 1] = rgba[1];
+      data[i + 2] = rgba[2];
+      data[i + 3] = rgba[3];
+    });
+    const layers = Array.from(skin.layers);
+    layers[i] = Object.assign(layers[i], { data: data });
+    return Object.assign(skin, { layers: layers });
+  },
+  upLayer(skin, i) {
+    if (i == 0) {
+      return skin;
+    }
+    const layers = Array.from(skin.layers);
+    const upper = layers[i - 1];
+    layers[i - 1] = layers[i];
+    layers[i] = upper;
+    return Object.assign(skin, { layers: layers });
+  },
+  downLayer(skin, i) {
+    if (i == skin.layers.length - 1) {
+      return skin;
+    }
+    const layers = Array.from(skin.layers);
+    const lower = layers[i + 1];
+    layers[i + 1] = layers[i];
+    layers[i] = lower;
+    return Object.assign(skin, { layers: layers });
+  },
+  toggleLayer(skin, i) {
+    const layers = Array.from(skin.layers);
+    layers[i] = Object.assign(layers[i], { visible: !layers[i].visible });
+    return Object.assign(skin, { layers: layers });
+  },
+  unshiftNewLayer(skin, label) {
+    const layers = Array.from(skin.layers);
+    const data = (new Array(skin.width * skin.height * 4)).map(function() { return 0; });
+    layers.unshift({ label: label, visible: true, data: data });
+    return Object.assign(skin, { layers: layers });    
+  }
 }
-
 
 class ImageEdit extends React.Component {
   constructor(props) {
@@ -82,55 +185,70 @@ class ImageEdit extends React.Component {
       prevPos: null,
       scale: 20
     }
+
+    this.onMouseDown = this.onMouseDown.bind(this);
+    this.onMouseMove = this.onMouseMove.bind(this);
+    this.onMouseUp   = this.onMouseUp.bind(this);
+    this.onMouseLeave= this.onMouseLeave.bind(this);
   }
 
+  putPixels(pixels) {
+    const skin = Skin.putPixels(
+      this.props.skin,
+      this.props.layerIndex,
+      pixels
+    )
+    this.props.changeSkin(skin)   
+  }
+
+  onMouseDown(e) {
+    e.preventDefault();
+    if (e.nativeEvent.which !== 1) { return }
+    const x = Math.floor(e.nativeEvent.offsetX / this.state.scale)
+    const y = Math.floor(e.nativeEvent.offsetY / this.state.scale)
+
+    this.setState({prevPos: [x, y]})
+    this.putPixels([[x, y, this.rgba()]])
+  }
+  
+  onMouseMove(e) {
+    e.preventDefault();
+    if (this.state.prevPos == null) { return }
+    if (e.nativeEvent.which !== 1) { return }
+
+    const x = Math.floor(e.nativeEvent.offsetX / this.state.scale)
+    const y = Math.floor(e.nativeEvent.offsetY / this.state.scale)
+
+    const px = this.state.prevPos[0]
+    const py = this.state.prevPos[1]
+
+    const pixels = []
+    pixels.push([x, y, this.rgba()])
+    const m = Math.abs(x - px) + Math.abs(y - py)
+    for (var i = 1; i < m; i++) {
+      const x1 = Math.floor(((m - i) * x + i * px) / m)
+      const y1 = Math.floor(((m - i) * y + i * py) / m)
+      pixels.push([x1, y1, this.rgba()])        
+    }
+    pixels.push([px, py, this.rgba()])
+
+    this.setState({prevPos: [x, y]})
+    this.putPixels(pixels)
+  }
+
+  onMouseUp(e) {
+    if (e.nativeEvent.which !== 1) { return }
+    this.setState({prevPos: null})
+  }
+
+  onMouseLeave(e) {
+    if (e.nativeEvent.which !== 1) { return }
+    this.setState({prevPos: null})
+  }
+  rgba() {
+    return this.props.color.concat([255]);
+  }
   render() {
-    let onMouseDown = (e) => {
-      e.preventDefault();
-      if (e.nativeEvent.which !== 1) { return }
-      const x = Math.floor(e.nativeEvent.offsetX / this.state.scale)
-      const y = Math.floor(e.nativeEvent.offsetY / this.state.scale)
-
-      const skin = putPixels(this.props.skin, 0, [[x, y, [255, 0, 255, 255]]])
-      this.setState({prevPos: [x, y]})
-      this.props.onSkinChange(skin)   
-    }
-
-    let onMouseMove = (e) => {
-      e.preventDefault();
-      if (this.state.prevPos == null) { return }
-      if (e.nativeEvent.which !== 1) { return }
-
-      const x = Math.floor(e.nativeEvent.offsetX / this.state.scale)
-      const y = Math.floor(e.nativeEvent.offsetY / this.state.scale)
-
-      const px = this.state.prevPos[0]
-      const py = this.state.prevPos[1]
-
-      const pixels = []
-      pixels.push([x, y, [255, 0, 255, 255]])
-      const m = Math.abs(x - px) + Math.abs(y - py)
-      for (var i = 1; i < m; i++) {
-        const x1 = Math.floor(((m - i) * x + i * px) / m)
-        const y1 = Math.floor(((m - i) * y + i * py) / m)
-        pixels.push([x1, y1, [255, 0, 255, 255]])        
-      }
-      pixels.push([px, py, [255, 0, 255, 255]])
-
-      const skin = putPixels(this.props.skin, 0, pixels)
-      this.setState({ prevPos: [x, y] })
-      this.props.onSkinChange(skin)   
-    }
-
-    let onMouseUp = (e) => {
-      if (e.nativeEvent.which !== 1) { return }
-      this.setState({prevPos: null})
-    }
-    let onMouseLeave = (e) => {
-      if (e.nativeEvent.which !== 1) { return }
-      this.setState({prevPos: null})
-    }
-
     const style = {
       width: (64 * this.state.scale) + 'px',
       height: (64 * this.state.scale) + 'px',
@@ -142,12 +260,18 @@ class ImageEdit extends React.Component {
 
     return (
       <div style={style}
-        onMouseDown={onMouseDown}
-        onMouseMove={onMouseMove}
-        onMouseUp={onMouseUp}
-        onMouseLeave={onMouseLeave}
+        onMouseDown={this.onMouseDown}
+        onMouseMove={this.onMouseMove}
+        onMouseUp={this.onMouseUp}
+        onMouseLeave={this.onMouseLeave}
       >
-        <ImageView skin={this.props.skin} width={(64 * this.state.scale)} height={(64 * this.state.scale)}/>
+        <p>{JSON.stringify(this.rgba())}</p>
+        <p>{JSON.stringify(this.props.color)}</p>
+        <ImageView
+          skin={this.props.skin}
+          scale={this.state.scale}
+          rendersGrid={true}
+        />
       </div>
     );
   }
@@ -160,8 +284,6 @@ const CsrfField = props => {
 }
 
 const SaveForm = props => (
-    
-
   <form action={Routes.editor_path()} method={"POST"}>
     <CsrfField />
     <input type={"hidden"} name={"skin"} value={JSON.stringify(props.skin)} />
@@ -171,23 +293,95 @@ const SaveForm = props => (
   </form>
 )
 
+const LayerEdit = (props) => {
+  const up = (i) => {
+    if (i == 0) { return; }
+    props.changeSkin(Skin.upLayer(props.skin, i), i - 1);
+    props.changeLayerIndex(i - 1);
+  };
+  const down = (i) => {
+    if (i == props.skin.length - 1) { return; }
+    props.changeSkin(Skin.downLayer(props.skin, i));
+    props.changeLayerIndex(i + 1);
+  };
+  const toggle = (i) => {
+    props.changeSkin(Skin.toggleLayer(props.skin, i));
+  };
+  const add = () => {
+    const label = prompt("新しいレイヤーの名前を入力してください", "");
+    if (label == null) { return; }
+    props.changeSkin(Skin.unshiftNewLayer(props.skin, label));
+  };
+
+  const items = props.skin.layers.map(function(l, i) {
+    return (
+        <div style={{borderBottom: "1px solid #ccc"}}>
+          <input type="radio" checked={i == props.layerIndex}
+                onClick={() => props.changeLayerIndex(i)}
+          />
+          <span onClick={() => props.changeLayerIndex(i)}>{l.label}</span>
+          <input type="checkbox" checked={l.visible} onClick={() => toggle(i)}/>
+          <span onClick={() => toggle(i)}>表示</span>
+          <button onClick={() => up(i)} disabled={i == 0}>↑</button>
+          <button onClick={() => down(i)} disabled={i == props.skin.layers.length - 1}>↓</button>
+        </div>
+    );
+  });
+  return (
+    <div>
+      <button onClick={() => add() }>新しいレイヤー</button>
+      <div style={{ width: '200px' }}>{items}</div>
+    </div>
+  );
+}
+
 class Editor extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
+
+    this.changeColorIndex = this.changeColorIndex.bind(this);
+    this.changeLayerIndex = this.changeLayerIndex.bind(this);
+    this.changeSkin = this.changeSkin.bind(this);
+
     this.state = {
-      skin: props.skin
+      skin: props.skin,
+      colors: props.colors,
+      colorIndex: 0,
+      layerIndex: 0
     }
   }
-  onSkinChange(skin) {
+  changeSkin(skin) {
     this.setState({skin: skin})
   }
+  changeColorIndex(i) {
+    this.setState({ colorIndex: i });
+  }
+  changeLayerIndex(i) {
+    this.setState({ layerIndex: i });
+  }
+
   render() {
+    const color = this.state.colors[this.state.colorIndex];
     return (
       <div>
+        <PaletteView
+          colors={this.state.colors}
+          colorIndex={this.state.colorIndex}
+          changeColorIndex={this.changeColorIndex}
+        />
+        <LayerEdit
+          skin={this.state.skin}
+          layerIndex={this.state.layerIndex}
+          changeLayerIndex={this.changeLayerIndex}
+          changeSkin={this.changeSkin}
+        />
         <SaveForm skin={this.state.skin} />
         <Preview skin={this.state.skin} />
-        <ImageEdit skin={this.state.skin}
-          onSkinChange={this.onSkinChange.bind(this)}
+        <ImageEdit
+          skin={this.state.skin}
+          color={color}
+          layerIndex={this.state.layerIndex}
+          changeSkin={this.changeSkin.bind(this)}
         />
       </div>
     )
@@ -196,7 +390,7 @@ class Editor extends React.Component {
 
 document.addEventListener('DOMContentLoaded', () => {
   const node = document.getElementById('editor')
-  const skin = JSON.parse(node.getAttribute('skin'))
+  const props = JSON.parse(node.dataset.props)
 
-  ReactDOM.render(<Editor skin={skin} />, node)
+  ReactDOM.render(<Editor skin={props.skin} colors={props.colors}/>, node)
 })
