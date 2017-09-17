1ec1da1cb6a0cbaa65f554a59d5098de59073b2a
OMOTO Kenji <doloopwhile@gmail.com>
2017-09-17 12:41:05 +0900

    tmp

---
+++ app/controllers/api/parsing_controller.rb
@@ -0,0 +1,32 @@
+class Api::ParsingController < ApplicationController
+  include Magick
+  
+  def png
+    p params
+    f = params.require(:file)
+    render json: { skin: skin_from_png(f.read) }
+  end
+
+  private
+  
+  def skin_from_png(png_data)
+    im = Image.read_inline(Base64.encode64(png_data)).first
+
+    data = im.rows.times.flat_map { |r| 
+      im.export_pixels(0, r, im.columns, 1, 'RGBA').map {|x| x / 256 }
+    }
+
+    {
+      width: 64,
+      height: 64,
+      layers: [
+        { label: 'あたま', data: data, visible: true, kind: :head },
+        { label: '上半身', data: data, visible: true, kind: :upper_body },
+        { label: '下半身', data: data, visible: true, kind: :lower_body },
+        { label: '帽子', data: data, visible: true, kind: :head_wear },
+        { label: '上のふく', data: data, visible: true, kind: :upper_body_wear },
+        { label: '下のふく', data: data, visible: true, kind: :lower_body_wear },
+      ]
+    }
+  end
+end
---
+++ app/javascript/packs/hello_react.jsx
@@ -0,0 +1,155 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+import Immutable from 'immutable'
+import Preview from './preview.jsx'
+import Palette from './palette.jsx'
+import Layers from './layers.jsx'
+import Skin from './skin.jsx'
+import ImageEdit from './image_edit.jsx'
+import SaveForm from './save_form.jsx'
+import ColorPicker from './color_picker.jsx'
+
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      skin: props.skin
+    }
+  }
+  changeSkin(skin) {
+    this.setState({ skin: skin });
+  }
+  render() {
+    if (this.state.skin == null) {
+      return <SkinUploadForm changeSkin={this.changeSkin.bind(this)}/>
+    } else {
+      return <Editor
+        skin={this.state.skin}
+        colors={this.props.colors}
+      />;
+    }
+  }
+}
+
+const SkinUploadForm = ({changeSkin}) => {
+  const ref = (element) => {
+    if (element == null) { return; }
+
+    $(element).submit((event) => {
+      event.preventDefault();
+      
+      var formdata = new FormData($(element).get(0));
+      $.ajax({
+        url: Routes.api_parsing_png_path('json'),
+        method: 'POST',
+        data: formdata,
+        cache: false,
+        contentType: false,
+        processData: false,
+        dataType: "json"
+      }).done((data, textStatus, jqXHR) =>{
+        changeSkin(data.skin);
+      });
+    });
+  }
+
+  return <form ref={ref}>
+    <input type="file" name="file"/>
+    <p>
+      <input type="submit" />
+    </p>
+  </form>;
+}
+
+class Editor extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      skin: props.skin,
+      colors: props.colors,
+      colorIndex: 0,
+      layerIndex: 0,
+      tool: "pen"
+    }
+  }
+  changeSkin(skin) {
+    this.setState({skin: skin});
+  }
+  changeColor(i, color) {
+    const colors = Array.from(this.state.colors);
+    colors[i] = color;
+    this.setState({ colors: colors });    
+  }
+  changeColorIndex(i) {
+    this.setState({ colorIndex: i });
+  }
+  changeLayerIndex(i) {
+    this.setState({ layerIndex: i });
+  }
+  changeTool(tool) {
+    this.setState({ tool: tool });
+  }
+  render() {
+    const color = this.state.colors[this.state.colorIndex];
+
+    let edit = [];
+    if (this.state.layerIndex != null) {
+      edit = [
+        <ImageEdit
+          skin={this.state.skin}
+          color={color}
+          colorIndex={this.state.colorIndex}
+          tool={this.state.tool}
+          changeTool={this.changeTool.bind(this)}
+          layerIndex={this.state.layerIndex}
+          changeSkin={this.changeSkin.bind(this)}
+          changeColor={this.changeColor.bind(this)}
+          scale={16}
+        />,
+      ];
+    }
+
+    return (
+      <div>
+        <SaveForm skin={this.state.skin} />
+        <Preview skin={this.state.skin} />
+        <div>
+          <div style={{float: 'left', width: '50%'}}>
+            <Layers
+              skin={this.state.skin}
+              layerIndex={this.state.layerIndex}
+              changeLayerIndex={this.changeLayerIndex.bind(this)}
+              changeSkin={this.changeSkin.bind(this)}
+            />
+          </div>
+          <div style={{float: 'left', width: '30%'}}>
+            <Palette
+              colors={this.state.colors}
+              colorIndex={this.state.colorIndex}
+              changeColorIndex={this.changeColorIndex.bind(this)}
+              changeColor={this.changeColor.bind(this)}
+            />
+          </div>
+          <div style={{float: 'left', width: '20%'}}>
+            <ColorPicker
+              colors={this.state.colors}
+              colorIndex={this.state.colorIndex}
+              changeColor={this.changeColor.bind(this)}
+            />
+          </div>
+        </div>
+        <hr />
+        {edit}
+      </div>
+    )
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const node = document.getElementById('editor')
+  const props = JSON.parse(node.dataset.props)
+  ReactDOM.render(<Main skin={props.skin} colors={props.colors}/>, node)
+})
